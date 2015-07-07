3931a74306ce102fc96776dd649861548fbbba82
Richard Xie <xieyijie@gmail.com>
2015-07-07 21:27:25 +1000

    fix db

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,18 +1,9 @@
 
-
-
-var DBURL =  OPENSHIFT_MONGODB_DB_URL || "mongodb://127.0.0.1:27017/propman";
-
-var DATASTORES = {
-  memory: {
-  },
-  mongodb: {
-    url: DBURL,
-    "name": "mongodb_dev",
-    "connector": "mongodb",
-  }
-};
-
 module.exports = {
-  db: DATASTORES
+  mongodb_dev: {
+    connector: 'mongodb',
+    url: process.env.OPENSHIFT_MONGODB_DB_URL,
+    database: 'propman',
+    name: "mongodb_dev"
+  }
 };
